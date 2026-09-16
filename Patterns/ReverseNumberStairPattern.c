836980f7176d1b0f07eeac088ea64d7eb859d158
Sanskriti Rana <sanskritikrana@gmail.com>
2026-09-16 22:55:37 +0530

    Reverse Number Stair Pattern

--- Patterns/ReverseNumberStairPattern.c
+++ Patterns/ReverseNumberStairPattern.c
@@ -1,7 +1,7 @@
 /*
 You are given a single integer N.
 Your task is to print a reverse number stair pattern using numbers from N to 1.
-
+ 
 The first row should contain numbers from N to 1
 Each next row should start from N-1, then N-2, and so on
 The last row should contain only 1
